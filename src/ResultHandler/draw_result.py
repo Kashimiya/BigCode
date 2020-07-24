21eb3a1f0bcf485cfe3a00ded2dbd0d05acedd90
Kos <729357028@qq.com>
2020-07-24 21:26:05 +0800

    why：为结果增加简单的聚类

--- src/ResultHandler/draw_result.py
+++ src/ResultHandler/draw_result.py
@@ -1,8 +1,28 @@
 import csv
 import os
 import numpy as np
+from sklearn.cluster import k_means
+from sklearn.metrics import silhouette_score
 from scipy.interpolate import spline
 from matplotlib import pyplot as plt
+
+
+# 为结果进行聚类
+def choose_clusters(nums):
+    data = []
+    for num in nums:
+        data.append([float(num), 0])
+    scores = []
+    for i in range(len(nums) - 2):
+        model = k_means(data, n_clusters=i + 2)
+        scores.append(silhouette_score(data, model[1]))
+    max_score = max(scores)
+    cluster = 0
+    for i in range(len(scores)):
+        if scores[i] == max_score:
+            cluster = i + 2
+            break
+    return k_means(data, cluster)
 
 
 def draw_result():
@@ -15,10 +35,12 @@ def draw_result():
         Y.append(float(line[1]))
     X = np.array(X)
     Y = np.array(Y)
+    model = choose_clusters(Y)
     # 散点图
-    plt.scatter(X, Y)
+    # 用颜色区分结果
+    plt.scatter(X, Y, c=model[1])
     plt.title("scatter plot for result")
-    plt.savefig(os.path.abspath('..\\..') + '\\img\\scatter plot for result.png')
+    # plt.savefig(os.path.abspath('..\\..') + '\\img\\scatter plot for result.png')
     plt.show()
     # 平滑曲线
     plt.figure()
@@ -26,7 +48,7 @@ def draw_result():
     newY = spline(X, Y, newX)
     plt.plot(newX, newY)
     plt.title("smooth curve for result")
-    plt.savefig(os.path.abspath('..\\..') + '\\img\\smooth curve for result.png')
+    # plt.savefig(os.path.abspath('..\\..') + '\\img\\smooth curve for result.png')
     plt.show()
 
 
